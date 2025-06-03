46f46ec5558e20009567f5e73ba7964358172a06
dboiago <dboiago@gmail.com>
2025-06-03 14:04:00 -0400

    Create AudiobookMetadata.cs

---
+++ Models/AudiobookMetadata.cs
@@ -0,0 +1,7 @@
+public class AudiobookMetadata
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string Narrator { get; set; }
+    public TimeSpan Duration { get; set; }
+}
